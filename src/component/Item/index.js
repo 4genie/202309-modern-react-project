f79e93900f7f7d8d5efceea3ecf3f3498847f252
4genie <forgenie4all@gmail.com>
2023-09-10 11:19:33 +0800

    style: add Add Button on Item component

--- src/component/Item/index.js
+++ src/component/Item/index.js
@@ -1,10 +1,30 @@
-const Item = ({ data }) => {
+import { PlusIcon, TrashIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
+const Item = ({ data = {} }) => {
   const image = data?.image;
   const title = data?.title;
   const price = data?.price;
 
+  const [count, setCount] = useState(0);
+
+  let isAddBtn = count > 0;
+
+  const handleAddBtnClick = (event) => {
+    event.stopPropagation;
+    setCount((prev) => prev + 1);
+  };
+
+  const handlePlusBtnClick = (event) => {
+    event.stopPropagation;
+    setCount((prev) => prev + 1);
+  };
+  const handleRemoveBtnClick = (event) => {
+    event.stopPropagation;
+    setCount((prev) => prev - 1);
+  };
+
   return (
-    <div className="mb-1  cursor-pointer bg-white p-4 transition-shadow hover:drop-shadow-lg">
+    <div className="relative mb-1  cursor-pointer bg-white p-4 transition-shadow hover:drop-shadow-lg">
       <div className=" relative flex aspect-video items-center overflow-hidden">
         <img src={image} alt={title} />
 
@@ -19,6 +39,27 @@ const Item = ({ data }) => {
       <div>
         <h3 className="text-lg">{title}</h3>
         <p className="text-slate-400">${price}</p>
+      </div>
+
+      <div className="absolute p-2 top-4 right-4 flex items-center gap-2 bg-black text-white rounded-full">
+        {!isAddBtn && (
+          <button onClick={handleAddBtnClick}>
+            <PlusIcon className="w-6 h-6" />
+          </button>
+        )}
+
+        {isAddBtn && (
+          <>
+            <button onClick={handleRemoveBtnClick}>
+              <TrashIcon />
+            </button>
+
+            <div>{count}</div>
+            <button onClick={handlePlusBtnClick}>
+              <PlusIcon />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
